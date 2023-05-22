52c4af3cf2f6d39a10780b3dce880a463d5a36fb
Leandro R.S <le.rsouza@gmail.com>
2023-05-21 21:39:07 -0300

    cheange tasks to better fit dataset sizes

--- lang_transfer/tasks.py
+++ lang_transfer/tasks.py
@@ -20,7 +20,6 @@ DEFAULT_PRE_PROCESSORS = [
     seqio.preprocessors.tokenize,
     preprocessing.group_texts,
     seqio.preprocessors.append_eos_after_trim,
-    preprocessing.take_n_tokens,
 ]
 
 ALL_LANGUAGES = (
@@ -28,42 +27,31 @@ ALL_LANGUAGES = (
     "es",
 )
 
-def get_dataset(split, shuffle_files=False, seed=None, language="en"):
-    """
-    Returns a `tf.data.Dataset` object with the mC4 data for the specified `language`.
-
-    This method leverages 2 strategies:
-
-    * It uses Huggingface's streaming capability to avoid download the whole mc4, which
-      is useful when building smaller datasets from it
-
-    * It encapsulates the HF's dataset library inside a tf.data.Dataset created from a generator.
-    """
-    dataset = load_dataset("mc4", language, split=split, streaming=True)
-
-    def _get_iterator():
-        """
-        This inner function only yields examples from the created `dataset`.
-        """
-        for item in dataset:
-            # print("one more")
-            yield item
-
-    return tf.data.Dataset.from_generator(
-        _get_iterator,
-        output_types={"text": tf.string, "timestamp": tf.string, "url": tf.string},
-    )
-
+DATASET_SIZES = {
+    "6M": 6291456,
+    "60M": 60293120,
+    "600M": 600309760,
+    "6B": 6000476160,
+}
 
 # ---------------- Language tasks -----------------
-for lang in ALL_LANGUAGES:
+
+for lang, (size_name, size) in itertools.product(ALL_LANGUAGES, DATASET_SIZES.items()):
     seqio.TaskRegistry.add(
-        f"langagnostic.{lang}",
-        source=seqio.FunctionDataSource(
-            dataset_fn=functools.partial(get_dataset, language=lang),
-            splits=["train", "validation"],
+        f"langagnostic.{lang}.{size_name}",
+        source=seqio.TFExampleDataSource(
+            {
+                "train": (
+                    f"gs://lang_agnostic/dataset/mc4_{lang}_train_10000000000.tfrecord"
+                ),
+                "validation": f"gs://lang_agnostic/dataset/mc4_{lang}_validation_309278350.tfrecord",
+            },
+            feature_description={
+                "text": tf.io.FixedLenFeature([], tf.string, default_value=""),
+            },
         ),
-        preprocessors=DEFAULT_PRE_PROCESSORS,
+        preprocessors=DEFAULT_PRE_PROCESSORS
+        + [functools.partial(preprocessing.take_n_tokens, n=size)],
         output_features=DEFAULT_BYTE_OUTPUT_FEATURES,
         metric_fns=[],
     )
