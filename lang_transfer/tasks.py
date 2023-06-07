b7540c2e711f7d243a015fd93c3f059698192a4b
Leandro R.S <le.rsouza@gmail.com>
2023-06-07 13:04:47 -0300

    using preprocessed datasets

--- lang_transfer/tasks.py
+++ lang_transfer/tasks.py
@@ -26,37 +26,45 @@ DEFAULT_PRE_PROCESSORS = [
     seqio.preprocessors.append_eos_after_trim,
 ]
 
+PASSTHROUGH_BYTE_OUTPUT_FEATURES = {
+    "inputs": seqio.Feature(vocabulary=seqio.PassThroughVocabulary(size=259), required=False),
+    "targets": seqio.Feature(vocabulary=seqio.PassThroughVocabulary(size=259)),
+}
+
+
+PASSTHROUGH_PREPROCESSORS = [
+]
+
 ALL_LANGUAGES = (
     "en",
     "es",
 )
 
-DATASET_SIZES = {
-    "6M": 6815744,
-    "60M": 60817408,
-    "600M": 600834048,
-    "6B": 6001000448,
-}
+DATASET_SIZES = [
+    "6M",
+    "60M",
+    "600M",
+    "6B",
+]
 
 # ---------------- Language tasks -----------------
 
 # ADD TRAIN datasets for all languages and sizes
 
-for lang, (size_name, size) in itertools.product(ALL_LANGUAGES, DATASET_SIZES.items()):
+for lang, size_name in itertools.product(ALL_LANGUAGES, DATASET_SIZES):
     seqio.TaskRegistry.add(
         f"langagnostic.{lang}.{size_name}",
         source=seqio.TFExampleDataSource(
             {
                 "train": (
-                    f"gs://lang_agnostic/dataset/mc4_{lang}_train_10000000000.tfrecord"
+                    f"gs://lang_agnostic/dataset/{lang}.{size_name}.examples"
                 ),
             },
             feature_description={
                 "text": tf.io.FixedLenFeature([], tf.string, default_value=""),
             },
         ),
-        preprocessors=DEFAULT_PRE_PROCESSORS
-        + [functools.partial(preprocessing.take_n_tokens, n=size)],
+        preprocessors=PASSTHROUGH_PREPROCESSORS,
         output_features=DEFAULT_BYTE_OUTPUT_FEATURES,
         metric_fns=[],
     )
@@ -74,26 +82,6 @@ for lang in ALL_LANGUAGES:
             },
         ),
         preprocessors=DEFAULT_PRE_PROCESSORS,
-        output_features=DEFAULT_BYTE_OUTPUT_FEATURES,
+        output_features=PASSTHROUGH_BYTE_OUTPUT_FEATURES,
         metric_fns=[],
-    )
-
-
-# Define a custom dataset function
-@gin.configurable(allowlist=["num_epochs", "continue_from_last_checkpoint"])
-def get_train_dataset_wrapper(
-    cfg: DatasetConfig,
-    shard_id: int,
-    num_shards: int,
-    feature_converter_cls: Callable[..., seqio.FeatureConverter],
-    num_epochs: Optional[int] = None,
-    continue_from_last_checkpoint: bool = False,
-) -> tf.data.Dataset:
-    return get_dataset(
-        cfg=cfg,
-        shard_id=shard_id,
-        num_shards=num_shards,
-        feature_converter_cls=feature_converter_cls,
-        num_epochs=num_epochs,
-        continue_from_last_checkpoint=continue_from_last_checkpoint,
     )
--- run_all_finetunes.sh
+++ run_all_finetunes.sh
@@ -27,7 +27,7 @@ FINETUNE_SIZES=("6M" "60M" "600M" "6B")
 FINETUNE_EPOCH_STEPS=(12 115 1145 11445)  # number of steps to form an epoch
 EPOCHS=(10 10 10 3)  # number of steps to form an epoch
 WARMUP_STEPS=(0 30 300 3000)
-MODEL_BASE_DIR="gs://lang_agnostic/models"
+MODEL_BASE_DIR="gs://lang_agnostic/models/finetune"
 PRETRAINED_MODEL_CHECKPOINT="gs://lang_agnostic/models/pretrained_${PRETRAINED_LANGUAGE}_${MODEL_SIZE}_6B/checkpoint_11445/"
 
 RUNS=${#FINETUNE_SIZES[@]}
@@ -53,8 +53,7 @@ for (( i=0; i<$RUNS; i++ )); do
         --gin.VAL_MIXTURE_OR_TASK_NAME=\""langagnostic.${LANGUAGE}.validation"\" \
         --gin.TRAIN_STEPS=${TRAIN_STEPS} \
         --gin.EVAL_PERIOD=${EVAL_PERIOD} \
-        --gin.WARMUP_STEPS=${WARMUP} \
-        --gin.TRAIN_DATASET_EPOCHS=${EPOCHS_TO_TRAIN}
+        --gin.WARMUP_STEPS=${WARMUP}
 
     if [ -n "$PRETRAINED_LANGUAGE" ]; then
 
@@ -69,7 +68,6 @@ for (( i=0; i<$RUNS; i++ )); do
           --gin.TRAIN_STEPS=${TRAIN_STEPS} \
           --gin.EVAL_PERIOD=${EVAL_PERIOD} \
           --gin.WARMUP_STEPS=0 \
-          --gin.PRETRAINED_MODEL_PATH=\"${PRETRAINED_MODEL_CHECKPOINT}\" \
-          --gin.TRAIN_DATASET_EPOCHS=${EPOCHS_TO_TRAIN}
+          --gin.PRETRAINED_MODEL_PATH=\"${PRETRAINED_MODEL_CHECKPOINT}\"
     fi
 done
