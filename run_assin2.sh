336d859b0d678bc461b4ed340670037da7cfc7d6
Leandro R.S <le.rsouza@gmail.com>
2024-03-29 23:09:09 -0300

    fix gin path and checkpointing location

--- run_assin2.sh
+++ run_assin2.sh
@@ -8,6 +8,7 @@ fi
 
 # Environment
 
+MODEL_SIZE="small"
 PROJECT_DIR="./lang_transfer/"
 T5X_DIR="./t5x"  # directory where the t5x is cloned.
 BUCKET_NAME=${BUCKET_NAME:="lang_agnostic_europe"}
@@ -20,7 +21,7 @@ PRETRAINED_MODEL_CHECKPOINT="gs://${BUCKET_NAME}/models/pretrained/pretrained_${
 
 python3 ${T5X_DIR}/t5x/train.py \
     --gin_search_paths=${PROJECT_DIR} \
-    --gin_file="lang_transfer/configs/runs/train_scratch.${MODEL_SIZE}.gin" \
-    --gin.MODEL_DIR=\"${MODEL_BASE_DIR}/assin2_from_${PRETRAINED_MODEL_CHECKPOINT}\" \
+    --gin_file="lang_transfer/configs/runs/assin2.gin" \
+    --gin.MODEL_DIR=\"${MODEL_BASE_DIR}/assin2_from_${PRETRAINED_LANGUAGE}\" \
     --gin.MIXTURE_OR_TASK_NAME=\""assin2"\" \
     --gin.INITIAL_CHECKPOINT_PATH=\"${PRETRAINED_MODEL_CHECKPOINT}\"
